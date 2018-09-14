3ea45f58def8acb258a836d7b5460fc5c84078ae
teamsterjr <alex@alixandalex.com>
2018-09-14 09:51:07 +0100

    Admin

--- ninty/initdb.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env python
-from ninty import init_db
-init_db()
--- script/gulpfile.js
+++ script/gulpfile.js
@@ -4,9 +4,16 @@ var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
 var jsDest = '../build/ninty/static/js/'
-
+var jsSrc = '../ninty/static/js/'
+gulp.task('dump', function () {
+  console.log(gulp.src([
+    jsDest + 'jquery.js',
+    jsDest + 'bootstrap.js',
+    jsDest + '**/*.js'
+  ]))
+})
 // Concatenate JS Files
-gulp.task('scripts', function() {
+gulp.task('scripts', function () {
   return gulp.src('../ninty/static/js/*.js')
     .pipe(concat('main.js'))
     .pipe(gulp.dest(jsDest))
