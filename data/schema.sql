1eb5e70967b42fa8badab5168c6b1edab3036053
Alex Knowles <alex.knowles@sapient.com>
2018-09-24 07:29:20 +0100

    dump db

--- data/schema.sql
+++ data/schema.sql
@@ -1,14 +1,13 @@
+-- MySQL dump 10.13  Distrib 8.0.12, for osx10.12 (x86_64)
 --
--- Table structure for table `game`
---
-
-DROP database IF EXISTS `EGFDS`;
-CREATE DATABASE `EGFDS`;
-use EGFDS;
+-- Host: localhost    Database: EGFDS
+-- ------------------------------------------------------
+-- Server version	8.0.12
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -17,10 +16,30 @@ use EGFDS;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `vote_id` int(11) NOT NULL,
+  `comment` text,
+  `promoted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  KEY `vote_id` (`vote_id`),
+  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`vote_id`) REFERENCES `vote` (`id`)
+) ENGINE=InnoDB;
+
+
+--
 -- Table structure for table `game`
 --
 
 DROP TABLE IF EXISTS `game`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+
 CREATE TABLE `game` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
@@ -30,11 +49,14 @@ CREATE TABLE `game` (
   CONSTRAINT `game_ibfk_1` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`)
 ) ENGINE=InnoDB;
 
+
 --
 -- Table structure for table `game_instance`
 --
 
 DROP TABLE IF EXISTS `game_instance`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+
 CREATE TABLE `game_instance` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `game_id` int(11) NOT NULL,
@@ -46,22 +68,28 @@ CREATE TABLE `game_instance` (
   CONSTRAINT `game_instance_ibfk_2` FOREIGN KEY (`platform_id`) REFERENCES `platform` (`id`)
 ) ENGINE=InnoDB;
 
+
 --
 -- Table structure for table `genre`
 --
 
 DROP TABLE IF EXISTS `genre`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+
 CREATE TABLE `genre` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
 
+
 --
 -- Table structure for table `platform`
 --
 
 DROP TABLE IF EXISTS `platform`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+
 CREATE TABLE `platform` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(40) NOT NULL,
@@ -69,10 +97,14 @@ CREATE TABLE `platform` (
   UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB;
 
+
 --
 -- Table structure for table `user`
 --
+
 DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(30) NOT NULL,
@@ -83,11 +115,14 @@ CREATE TABLE `user` (
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB;
 
+
 --
 -- Table structure for table `vote`
 --
 
 DROP TABLE IF EXISTS `vote`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+
 CREATE TABLE `vote` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `vote` tinyint(2) NOT NULL DEFAULT '0',
@@ -102,30 +137,6 @@ CREATE TABLE `vote` (
   CONSTRAINT `vote_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB;
 
---
--- Table structure for table `comment`
---
-
-DROP TABLE IF EXISTS `comment`;
-CREATE TABLE `comment` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `vote_id` int(11) NOT NULL,
-  `comment` text,
-  `promoted` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `vote_id` (`vote_id`),
-  CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`vote_id`) REFERENCES `vote` (`id`)
-) ENGINE=InnoDB;
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -135,3 +146,4 @@ CREATE TABLE `comment` (
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
