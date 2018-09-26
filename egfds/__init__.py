75f1a7cb63358b7a86b31a856f7044fbb2a72d25
teamsterjr <alex@alixandalex.com>
2018-09-26 17:20:41 +0100

    tidy up build scripts

--- egfds/__init__.py
+++ egfds/__init__.py
@@ -19,6 +19,7 @@ def create_app(test_config=None):
     if test_config is None:
         # load the instance config, if it exists, when not testing
         app.config.from_pyfile("config.py")
+        app.config.from_pyfile("secret.py")
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
